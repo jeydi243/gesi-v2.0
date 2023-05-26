bfad162c1b46226b4ec92220871e452781086dd3
jeydi243 <ilungakadiongo@gmail.com>
2023-05-27 01:11:10 +0200

    on evolue

--- src/components/side.vue
+++ src/components/side.vue
@@ -5,50 +5,66 @@
 			<button @click="rem" class="btn-unstate">remove</button> -->
 			<!-- <div id="jog" :class="`bg-red-500 rounded-md h-8 w-10 relative z-0 transition-all duration-500 ease-in-out`"></div> -->
 			<TransitionGroup name="fadeSlideY">
-				<router-link data-splitting :to="item.to" @click="config.changeActive(item.to)" @hover="item.mouseHover = !item.mouseHover" :id="item.text" :ref="item.text" v-for="(item, index) in sideMenus" :data-index="index" :key="index" class="router-link relative z-1" :class="{ 'router-link-active': item.active }">
-					<box-icon type="regular" :name="item.icon" color="white"></box-icon>
-					<span class="ml-2 text-sm font-medium">{{ item.text }}</span>
-				</router-link>
+				<template v-for="(item, index) in sideMenus">
+					<details v-if="item.children">
+						<summary class="ml-2 text-sm font-medium">
+							<router-link :to="item.to" @click="config.changeActive(item.to)" @hover="item.mouseHover = !item.mouseHover" :id="item.text" :ref="item.text" :data-index="index" :key="index" class="router-link relative z-1" :class="{ 'router-link-active': item.active }">{{ item.text }}
+								<span class="justify-self-end ml-auto text-xs">lklm</span>
+							</router-link>
+						</summary>
+						<template v-for="(sub, index2) in item.children">
+							<router-link :to="sub.to">
+								{{ sub.text }}
+							</router-link>
+						</template>
+					</details>
+					<router-link v-else :to="item.to" @click="config.changeActive(item.to)" @hover="item.mouseHover = !item.mouseHover" :id="item.text" :ref="item.text" :data-index="index" :key="index" class="router-link relative z-1 flex row" :class="{ 'router-link-active': item.active }">
+						<box-icon type="regular" :name="item.icon" color="white"></box-icon>
+						<span class="ml-2 text-sm font-medium">{{ item.text }}</span>
+
+					</router-link>
+				</template>
+
 			</TransitionGroup>
 		</div>
 	</div>
 </template>
 
-<script setup>
-	import { beforeEnterList, enterList, leaveList } from "@/utils/index"
-	import { useElementBounding } from "@vueuse/core"
-	import { computed, ref, onMounted, watch } from "vue"
-	import { useConfig } from "@/store/config"
-	import Splitting from "splitting"
-	import { gsap } from "gsap"
-	const config = useConfig()
-	const sideMenus = computed(() => config.sideMenus)
-	const sideActive = computed(() => config.sideActive)
-	const fg = ref(null)
-	const jh = ref(null)
-	const results = ref(null)
+<script setup lang="ts">
+import { beforeEnterList, enterList, leaveList } from "@/utils/index"
+import { useElementBounding } from "@vueuse/core"
+import { computed, ref, onMounted, watch } from "vue"
+import { useConfig } from "@/store/config"
+import Splitting from "splitting"
+import { gsap } from "gsap"
+const config = useConfig()
+const sideMenus = computed(() => config.sideMenus)
+const sideActive = computed(() => config.sideActive)
+const fg = ref(null)
+const jh = ref(null)
+const results = ref(null)
 
-	watch(sideActive, function (newval, oldval) {
-		// const jog = document.getElementById("jog")
-		if (newval != oldval) {
-			const el = document.getElementById(newval.text)
-			const { left, top } = useElementBounding(el)
-			// console.log(left.value, top.value)
-			// jog.style.top = `${top.value}px`
-			// jog.style.left = `${left.value}px`
-			// fg.value = top.value
-			// jh.value = left.value
-			// jog.classList.add(`top-[${top.value}px]`)
-			// jog.classList.add(`left-[${left.value}px]`)
-		}
-	})
+watch(sideActive, function (newval, oldval) {
+	// const jog = document.getElementById("jog")
+	if (newval != oldval) {
+		const el = document.getElementById(newval.text)
+		const { left, top } = useElementBounding(el)
+		// console.log(left.value, top.value)
+		// jog.style.top = `${top.value}px`
+		// jog.style.left = `${left.value}px`
+		// fg.value = top.value
+		// jh.value = left.value
+		// jog.classList.add(`top-[${top.value}px]`)
+		// jog.classList.add(`left-[${left.value}px]`)
+	}
+})
 
-	function add() {
-		config.add()
-	}
-	function rem() {
-		config.rem()
-	}
+function add() {
+	config.add()
+}
+function rem() {
+	config.rem()
+}
 </script>
 
 <style lang="scss" scoped></style>
